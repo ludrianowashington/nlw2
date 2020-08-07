2d1c6c539d60942448e8c0f3264a2ab7b0827f49
ludrianowashington <ludriano.washi@gmail.com>
2020-08-07 03:47:38 -0300

    Finalizando o frontend

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,34 +3,53 @@ import React from 'react'
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
+import api from '../../services/api'
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+};
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars2.githubusercontent.com/u/53495653?s=460&u=b9edd9e0693429806281ec1a93823dc551b56164&v=4" alt="Ludriano Washington" />
+        <img src={teacher.avatar} alt={teacher.name} />
 
         <div>
-          <strong>Ludriano Washington</strong>
-          <span>Matemática</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
       <p>
-        Entusiasta das melhores metodologias de matemática avançada.
-            <br /><br />
-            Apaixonado por explosões matemáticas e por mudar a vida das pessoas através de suas experiências.
-          </p>
+        {teacher.bio}
+      </p>
 
       <footer>
         <p>
           Preço/Hora
-              <strong>R$ 60,00</strong>
+              <strong>{teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="whatsapp" />
               Entrar em contato
-            </button>
+            </a>
       </footer>
 
     </article>
